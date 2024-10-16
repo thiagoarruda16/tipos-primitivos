33feb7359331b8e698a8cfe642e08e31021edcef
Thiago Arruda <thiagoarruda791@gmail.com>
2024-10-16 15:28:00 -0300

    first commimt

---
+++ aula1.js
@@ -0,0 +1,10 @@
+const pesquisa = "maça"
+
+switch(pesquisa){
+    case "maça": console.log("A maça custa R$ 5,00");break;
+    case "limao": console.log("O limao custa R$ 5,00");break;
+    default: console.log("Essa fruta esta indisponivel");
+    
+}
+
+alert("Boas compras!")
